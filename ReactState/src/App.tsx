6ae110ed3c134f7d6655331ff34c7207675185d0
Mehtab <mehtab185198131@gmail.com>
2026-03-30 11:02:32 +0530

    Fix import path for Todos component

--- ReactState/src/App.tsx
+++ ReactState/src/App.tsx
@@ -2,7 +2,7 @@
 // import { Greeting } from '/components/Greeting';
 // import { ItemsList as NavMenu } from '/components/ItemsList';
 // import { UserDashboard as UserProfile } from '/components/UserDashboard';
-import { Todos } from './components/Todos';
+import { Todos } from '/components/Todos';
 
 export default function App() {
   return (
